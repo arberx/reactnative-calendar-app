57aa5028083ffd197724c4a660bf2db3352cf628
axhindol <axhindol@umich.edu>
2017-08-01 22:06:12 -0700

    Calendar view working, need to figure out how to populate with dynamic data

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { headerCal, calendarContent } from './components/common';
+import { Container } from 'native-base';
+import { HeaderCal, CalendarContent } from './components/common';
 
 class App extends Component {
     render() {
         return (
-            // <View>
-            // // figure out the layout
-            // <headerCal headerText="CalendAR" />
-            // render this if user has created a calendar, and viewing it
-            <calendarContent />
-            // </View>
+            <Container>
+                <HeaderCal headerText="CalendAR" />
+                <CalendarContent />
+            </Container>
         );
     }
 }
