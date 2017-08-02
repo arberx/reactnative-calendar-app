0148dcca5e34d67886bb1934a67b68cf03bc2478
axhindol <axhindol@umich.edu>
2017-08-01 20:39:53 -0700

    started to play around with wix calendars

--- src/components/common/HeaderCal.js
+++ src/components/common/HeaderCal.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Title } from 'native-base';
 
-
-class HeaderCal extends Component {
+class headerCal extends Component {
     render() {
         return (
+            <Container>
                 <Header>
                     <Left>
                     <Body style={{ justifyContent: 'center' }}>
@@ -12,9 +12,10 @@ class HeaderCal extends Component {
                     </Body>
                     </Left>
                 </Header>
+            </Container>
         );
     }
 }
 
 //export if you use export *, must be an object
-export { HeaderCal };
+export { headerCal };
